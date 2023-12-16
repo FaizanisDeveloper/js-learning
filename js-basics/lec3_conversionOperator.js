db038825038197f45e55061290ebfe1e09cf8cbd
FaizanisDeveloper <153933302+FaizanisDeveloper@users.noreply.github.com>
2023-12-16 18:05:25 +0000

    Datatype Conversion

---
+++ js-basics/lec3_conversionOperator.js
@@ -0,0 +1,33 @@
+let age=39
+//console.log(typeof age) //Number
+age="39"
+//console.log(typeof age) //string
+let stringToNumber=Number(age)
+//console.log(typeof stringToNumber)//number
+age="39abc"
+let ageinNumber=Number(age) 
+////console.log(typeof ageinNumber) //number
+//console.log(ageinNumber)    //NaN
+age=null
+let nulltoNumber=Number(age)
+//console.log(typeof nulltoNumber) //number
+age=undefined
+let undefinedtoNumber=Number(age)
+//console.log(typeof undefinedtoNumber) //number
+//console.log(undefinedtoNumber)//NaN
+age=true
+//console.log(age)    //true
+let booltoNumber=Number(age)
+//console.log(typeof booltoNumber) //number
+//console.log(booltoNumber)   //1
+let isLoggedIn=1
+let boolisLoggedIn=Boolean(isLoggedIn)
+console.log(boolisLoggedIn) //true
+isLoggedIn=""
+let stringtoBool=Boolean(isLoggedIn)
+console.log(stringtoBool)
+console.log(typeof stringtoBool)
+isLoggedIn="hello"
+let filledStringtoBool=Boolean(isLoggedIn)
+console.log(typeof filledStringtoBool)
+console.log(filledStringtoBool)
