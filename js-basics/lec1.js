8d249dcf427fa9e463300f619175e3e0ef18fbfc
FaizanisDeveloper <153933302+FaizanisDeveloper@users.noreply.github.com>
2023-12-16 05:09:34 +0000

    comit done

--- js-basics/lec1.js
+++ js-basics/lec1.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+//Variable Type in Javascript
+const studentId=1234
+let studentName="Muhammad"
+var studentMail="muhammadali@gmail.com"
+studentGender="Male"
+let studentAddress;
+
+//studentId=49939   Not Allowed!
+studentName="Ali Raza"
+studentMail="aliraza@gmail.com"
+
+//I will use let in my JS coding in future
+
+{
+    studentName="Faizan"
+    studentMail="49939@students.riphah.edu.pk"
+}
+console.log([studentName,studentMail])
+
+console.log([studentId,studentName,studentMail])
+
+//Table
+console.table([studentId,studentMail,studentName,studentGender,studentAddress])
