19777f846e9e9afa44f608667c9c755e112615f3
FaizanisDeveloper <153933302+FaizanisDeveloper@users.noreply.github.com>
2023-12-17 13:14:38 +0000

    Comparison Operators

---
+++ js-basics/lec4_comparisonOperator.js
@@ -0,0 +1,24 @@
+// **Operation's topic is also included in this 
+
+// num<num
+// num>num
+// num<=num
+// num>=num
+// num!=num
+// num==num
+// num===num   //Strictly check datatype
+
+// console.log(3<4)
+// console.log(4>5)
+// console.log("4"<10)
+// console.log("4"<<10)
+// console.log(null>=0)
+// console.log(null<0.9)
+// console.log(null>10)
+// console.log(undefined<=0)
+// console.log(undefined<=0)
+// console.log(undefined==0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+// console.log("2" + 2 + 2)
+// console.log(2 + 2 + "1")
