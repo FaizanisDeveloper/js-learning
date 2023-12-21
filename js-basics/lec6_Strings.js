497eec804fad3e4a737d75524b74ef3ca7e82c71
FaizanisDeveloper <153933302+FaizanisDeveloper@users.noreply.github.com>
2023-12-21 12:09:25 +0000

    String in JS

---
+++ js-basics/lec6_Strings.js
@@ -0,0 +1,6 @@
+let name= "Ali"
+const repoCount=20
+// console.log("Hello my name is " + name + repoCount)
+
+// Suitable way to Concatenate strings
+console.log(`Hello my name is ${name} and my RepoCount is ${repoCount}`)
