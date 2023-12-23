2589aba811123c101c9bc2a5d52d2a8b87aafe2b
FaizanisDeveloper <153933302+FaizanisDeveloper@users.noreply.github.com>
2023-12-23 21:15:03 +0000

    Strings in JS

--- js-basics/lec6_Strings.js
+++ js-basics/lec6_Strings.js
@@ -3,4 +3,15 @@ const repoCount=20
 // console.log("Hello my name is " + name + repoCount)
 
 // Suitable way to Concatenate strings
-console.log(`Hello my name is ${name} and my RepoCount is ${repoCount}`)
+// console.log(`Hello my name is ${name} and my RepoCount is ${repoCount}`)
+
+const var_Name = new String("Faizan")
+// console.log(var_Name)
+// console.log(var_Name[2])
+// console.log(var_Name.length)
+// console.log(var_Name.toUpperCase())
+// console.log(var_Name.toLowerCase())
+// console.log(var_Name.charAt(2))
+// console.log(var_Name.charAt(0))
+// console.log(var_Name.indexOf('i'))
+console.log(var_Name.search(3))
